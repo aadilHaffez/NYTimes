c8c13c890cfd86b732dd740c8908038a8b21b729
aadilkhaskheli <>
2020-01-25 14:11:44 +0400

    Project Created

---
+++ NYTimesMauqah/Bootstrapper.swift
@@ -0,0 +1,56 @@
+//
+//  Bootstrapper.swift
+//  NYTimesMauqah
+//
+//  Created by aadilkhaskheli on 25/01/2020.
+//  Copyright © 2020 aadilkhaskheli. All rights reserved.
+//
+
+import Foundation
+import UIKit
+struct Bootstrapper {
+    var window : UIWindow
+    static var instance : Bootstrapper? = nil
+    private init(window: UIWindow) {
+        self.window = window
+    }
+    
+    static func initialize() {
+        
+       // NetworkManager.shared.startListening()
+        
+        instance = Bootstrapper(window: makeNewWindow())
+        instance!.bootstrap()
+    }
+    
+    private static func makeNewWindow() -> UIWindow {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.backgroundColor = UIColor.white
+        
+        if let del = UIApplication.shared.delegate {
+            (del as! AppDelegate).window = window
+        }
+        
+        return window
+    }
+    
+    mutating func bootstrap() {
+        showNewsListing()
+        window.makeKeyAndVisible()
+    }
+    
+    private func showNewsListing() {
+
+        let navigationController = UINavigationController()
+
+        /* Create ui-view-controller instance*/
+        
+        let newsListing = UIViewController()//NewsListingRouter.createModule(using: navigationController)
+
+        /* Initiating instance of ui-navigation-controller with view-controller */
+        navigationController.viewControllers = [newsListing]
+
+        /* Setting up the root view-controller as ui-navigation-controller */
+        window.rootViewController = navigationController
+    }
+}
